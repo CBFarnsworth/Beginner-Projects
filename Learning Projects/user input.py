e1854928edc6c62310c01942ef13ed54ba43442b
Brooklyn_ <124331343+CBFarnsworth@users.noreply.github.com>
2024-03-10 21:17:09 -0600

    Add files via upload

---
+++ Learning Projects/user input.py
@@ -0,0 +1,20 @@
+
+name1 = input("Enter your name: ")
+verb1 = input("Enter a verb: ")
+emotion1 = input("Enter an emotion: ")
+animal1 = input("Enter an animal: ")
+verb2 = input("Enter another verb: ")
+friend1 = input("Enter one of your friend's names: ")
+verb3 = input("Enter another verb: ")
+verb4 = input("Enter one more verb: ")
+
+#The Story
+
+print(f"Hello, I am {name1}!")
+print(f"I like to {verb1}.")
+print(f"When I {verb1} I feel very {emotion1}.")
+print(f"On the other hand, I do not like to {verb2}.")
+print(f"{verb2} makes me sad, but it makes {friend1} incredibly happy!")
+print(f"{friend1} participates in {verb2} almost daily.")
+print(f"They cannot go a day without {verb2}, or else they will {verb3}")
+print(f"I wish I could be as happy as {friend1} when I tried {verb2}, but for now I will try {verb4}")
